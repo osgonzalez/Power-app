401f3659cd1e2d61399903f0d85c71e58e9abf3f
Eldam <mbmartin@esei.uvigo.es>
2017-11-16 19:18:27 +0100

    Add the user model
    	-Add User.php file

---
+++ Models/User.php
@@ -0,0 +1,145 @@
+<?php 
+
+
+/**
+* 
+*/
+class User
+{
+	private $DNI;
+    private $UserType;
+    private $PasswordHash;
+    private $FirstName;
+    private $LastName;
+    private $Email;
+    private $Telephone;
+    private $City;
+    private $Birthdate;
+
+
+    /**
+     * User constructor.
+     * @param $DNI
+    */
+    public function __construct($DNI)
+    {
+        $this->DNI = $DNI;
+    }
+
+    /**
+     * User constructor.
+     * @param $DNI
+     * @param $UserType
+     * @param $PasswordHash
+     * @param $FirstName
+     * @param $LastName
+     * @param $Email
+     * @param $Telephone
+     * @param $City
+     * @param $Birthdate
+     */
+    public function loadDataAssoc($dataArray)
+    {
+        $result = $dataArray->fetch_assoc();
+        $this->UserType = $result['UserType'];
+        $this->PasswordHash = $result['PasswordHash'];
+        $this->FirstName = $result['FirstName'];
+        $this->LastName = $result['LastName'];
+        $this->Email = $result['Email'];
+        $this->Telephone = $result['Telephone'];
+        $this->City = $result['City'];
+        $this->Birthdate = $result['Birthdate'];
+    }
+
+    public function loadData($UserType, $PasswordHash, $FirstName, $LastName, $Email, $Telephone, $City, $Birthdate)
+    {
+
+        $this->UserType = $UserType;
+        $this->PasswordHash = $PasswordHash;
+        $this->FirstName = $FirstName;
+        $this->LastName = $LastName;
+        $this->Email = $Email;
+        $this->Telephone = $Telephone;
+        $this->City = $City;
+        $this->Birthdate = $Birthdate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDNI()
+    {
+        return $this->DNI;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserType()
+    {
+        return $this->UserType;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPasswordHash()
+    {
+        return $this->PasswordHash;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->FirstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->LastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->Email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTelephone()
+    {
+        return $this->Telephone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCity()
+    {
+        return $this->City;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBirthdate()
+    {
+        return $this->Birthdate;
+    }
+
+
+
+
+}
+
+
+ ?>
