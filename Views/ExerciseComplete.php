169485e38d1a7894beed18a262672526cfa72fd1
Raquel Marcos <rmenriquez@esei.uvigo.es>
2017-11-29 19:08:56 +0100

    TableShowExercisesView completed
    Vista de todos los ejercicios de una tabla completada, creación de
    vista de ejercicio de una tabla.

---
+++ Views/ExerciseComplete.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: RaquelMarcos
+ * Date: 29/11/17
+ * Time: 19:06
+ */
+class ExerciseComplete{
+
+
+}
--- Views/TableShowExercisesView.php
+++ Views/TableShowExercisesView.php
@@ -32,42 +32,20 @@ class TableView{
                     <tbody>
                     <?php
 
-                    foreach ($this->table->getExercises() as $exercise){ ?>
+                    foreach ($this->table->getExercises() as $exercise){
+                        ?>
                         <tr>
-                            <td><?php echo $exercise->getUrlImage();?></td>
+                            <td><a><img src="<?php echo $exercise->getUrlImage();?>" width="90px" height="60px"></a></td>
                             <td><?php  echo $exercise->getName();?></td>
                             <td><?php  echo $exercise->getExerciseType();?></td>
                             <td><?php  echo $exercise->getContent();?></td>
 
 
 
-                            <td><?php //echo '<a href=""><img src="../Templates/img2/edit.png" style="width:30px;height:30px;" title="Editar"></a>';?></td>
-                            <td><?php //echo '<a href="../Controllers/TableActionController.php?action=DELETE&IDTable='.$table->getIDTable().'"><img src="../Templates/img2/delete.png" style="width:30px;height:30px;" title="Borrar"></a>';?></td>
+                            <td><?php echo '<a href="ExerciseComplete.php" target="_blank" onclick="window.open(this.href, this.target, \'width=300,height=400\'); return false;"><img src="../Templates/img2/ver.png" style="width:30px;height:20px;" title="Ver ejercicio"></a>';?></td>
 
                         </tr>
-                        <div class="exerciseDetails">
-                            <div class="control-menu">
-                                <a href="#exerciseDetails" class="openTabla"><span>Ver ejericios</span></a>
-                                <a href="#" class="closeTabla"><span>Cerrrar ejercicios</span></a>
-                            </div>
-                            <table class="table-items">
-                                <tr>
-                                    <th class="hidden-phone">Ejercicio</th>
-                                    <th class="hidden-phone">Nombre</th>
-                                    <th class="hidden-phone">Tipo de ejercicio</th>
-                                    <th class="hidden-phone">Video</th>
-                                    <th class="hidden-phone">Descripción</th>
-                                </tr>
-                                <tr>
-                                    <td><?php echo $exercise->getUrlImage();?></td>
-                                    <td><?php  echo $exercise->getName();?></td>
-                                    <td><?php  echo $exercise->getExerciseType();?></td>
-                                    <td><?php  echo $exercise->getUrlVideo();?></td>
-                                    <td><?php  echo $exercise->getContent();?></td>
-                                </tr>
-                            </table>
-
-                        </div><?php
+                        <?php
                     }
                     ?>
                     </tbody>
