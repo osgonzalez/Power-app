d7e0327eadd5739ba0de759cc44f09f1007551a0
Arinc93 <arinc93@gmail.com>
2018-01-13 23:45:45 +0100

    NotificationShowAllController Created

---
+++ Controllers/NotificationsShowAllController.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+include '../Functions/Authentication.php';
+
+if (!IsAuthenticated()){
+
+    header('Location:../index.php');
+
+}else{
+
+  include '../Models/NotificationModel.php';
+  include '../Models/Notification.php';
+  $DAO = new NotificationDAO();
+
+  $message = $DAO->getAll();
+
+  if(strcasecmp($message,"ok") != 0){
+
+      include '../Views/MESSAGE_View.php';
+      new MESSAGE($message,'../index.php');
+
+  }else{
+
+      include '../Views/NotificationShowAllView.php';
+      new NotificationShowAllView($DAO->getLastResult());
+  }
+
+}
+
+
+
+?>
